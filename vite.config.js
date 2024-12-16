47ef719bba9cc5cde5d63ce141f8e8d30687bd9d
Giannis Nikoloulis <p18niko@ionio.gr>
2024-12-16 14:15:14 +0200

    changed base back to ./

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: '/app/',
+    base: './',
     server:
     {
         host: true,
