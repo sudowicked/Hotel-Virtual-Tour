96e371a98ac8a7e0ea6eb372d9f79813c0478da1
Giannis Nikoloulis <p18niko@ionio.gr>
2025-01-16 13:15:42 +0200

    apo click animation

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { gsap } from 'gsap/gsap-core';
 
 // Scene
 const scene = new THREE.Scene();
 
-// Camera
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+
+// Mouse
+const mouse = new THREE.Vector2();
+
+// Camera
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1;
+    mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 100);
-camera.position.set(0, -1, 4);
+camera.position.set(0, -1, 7);
 scene.add(camera);
 
 // Renderer
@@ -21,7 +31,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
-renderer.setClearColor(0xBC79FF);
+renderer.setClearColor(0x160000);
 
 /**
  * MAIN CODE
@@ -30,27 +40,36 @@ renderer.setClearColor(0xBC79FF);
 // Meshes
 const floor = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshStandardMaterial({color: 0x6B6B6B}));
 floor.rotation.x = - Math.PI * .5;
-floor.position.y = -2;
+floor.position.y = -4;
 scene.add(floor);
 
 const modelLoader = new GLTFLoader();
+let model = null;
 let mixer = null;
+let action = null;
+let currentIntersect = null;
 
 modelLoader.load(
   "./meshes/apoman.glb",
-  (model) =>
+  (gltf) =>
   {
-    mixer = new THREE.AnimationMixer(model.scene);
-    const action = mixer.clipAction(model.animations[0]);
-    action.play();
-    
-    scene.add(model.scene.children[0]);
+    model = gltf.scene;
+    mixer = new THREE.AnimationMixer(gltf.scene);
+    action = mixer.clipAction(gltf.animations[0]);
+    action.setLoop( THREE.LoopOnce );
+    action.clampWhenFinished = true; 
+    scene.add(model);
   }
 );
 
+// Raycaster
+const raycaster = new THREE.Raycaster();
+
 // Lights
-const ambient = new THREE.AmbientLight(0xBC79FF, 1);
-scene.add(ambient);
+const ambient = new THREE.AmbientLight(0x160000, .8);
+const rectLight = new THREE.RectAreaLight(0xE60000, 2, 4, 4);
+rectLight.position.z = 7;
+scene.add(ambient, rectLight);
 
 // Orbit controls
 const controls = new OrbitControls(camera, canvas);
@@ -79,7 +98,45 @@ window.addEventListener('dblclick', () => {
     else {
         document.exitFullscreen();
     }
-})
+});
+
+let inFocus = false;
+// Mouse click function
+window.addEventListener('click', () => {
+    if (currentIntersect) {
+        if (!inFocus) {
+            // Animation handling
+            gsap.to(model.scale, {duration:1, x:2});
+            gsap.to(model.scale, {duration:1, y:2});
+            gsap.to(model.scale, {duration:1, z:2});
+        
+            gsap.to(model.position, {duration:1, z:2});
+
+            gsap.to(model.rotation, {duration:2, x:Math.PI * .15});
+            
+            // Facial animation handling
+            inFocus = true;
+            action.timeScale = 1;
+            action.reset();
+            action.play();
+        }
+        else {
+            gsap.to(model.scale, {duration:1, x:1});
+            gsap.to(model.scale, {duration:1, y:1});
+            gsap.to(model.scale, {duration:1, z:1});
+
+            gsap.to(model.position, {duration:1, z:-2});
+
+            gsap.to(model.rotation, {duration:2, x: 0});
+
+            inFocus = false;
+            action.reset();
+            action.time = 2;
+            action.timeScale = -1;
+            action.play();
+        }
+    }
+});
 
 const clock = new THREE.Clock();
 let previousTime = 0;
@@ -92,7 +149,21 @@ function tick() {
 
     if (mixer) {
         mixer.update(deltaTime);
-    }
+    };
+
+    // Raycast
+    raycaster.setFromCamera(mouse, camera);
+
+    // Check if model is being hovered
+    if (model) {
+        const modelIntersects = raycaster.intersectObject(model);
+        if (modelIntersects.length) {
+            currentIntersect = modelIntersects;
+        }
+        else {
+            currentIntersect = null;
+        }
+    };
     
     controls.update();
     renderer.render(scene, camera);
