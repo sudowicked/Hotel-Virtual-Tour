73c038e5b31949cf6e9493e5308669e9d49c1c45
Giannis Nikoloulis <p18niko@ionio.gr>
2025-01-28 22:41:27 +0200

    Updates

---
+++ libs/JSONHelper.js
@@ -0,0 +1,13 @@
+export function loadJSON(path) {
+	return new Promise((resolve, reject) => {
+	  fetch(path)
+		.then(response => response.json())
+		.then(json => {
+		  resolve(json);
+		})
+		.catch(error => {
+		  reject(error);
+		  console.log('error')
+		});
+	});
+}
