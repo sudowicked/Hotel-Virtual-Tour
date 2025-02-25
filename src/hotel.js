e443a38263530ad364249f733126b78631c56559
Giannis Nikoloulis <p18niko@ionio.gr>
2025-02-25 13:07:27 +0200

    panasonic site

--- src/hotel.js
+++ src/hotel.js
@@ -7,13 +7,18 @@ import PositionAlongPathState from '../static/libs/positionAlongPathTools/Positi
 import { handleScroll, updatePosition, isScrolling} from '../static/libs/positionAlongPathTools/PositionAlongPathMethods';
 import * as dat from 'lil-gui';
 import { setupRenderer } from '../static/libs/RendererHelper';
+import { RenderPass } from 'three/examples/jsm/Addons.js';
+import { OutputPass } from 'three/examples/jsm/Addons.js';
+import { UnrealBloomPass } from 'three/examples/jsm/Addons.js';
+import { EffectComposer } from 'three/examples/jsm/Addons.js';
+import { RectAreaLightHelper } from 'three/examples/jsm/Addons.js';
 
 // Scene
 const scene = new THREE.Scene();
 
 // Paths
-const hotelPath = './meshes/hotelRoom/room_final.glb'
-const curvePathJSON = './meshes/hotelRoom/roomPath.json'
+const hotelPath = './meshes/panasonic/EditorFinal.glb'
+const curvePathJSON = './meshes/panasonic/PanaCurve.json'
 
 const sizes = {
     width: window.innerWidth,
@@ -31,6 +36,9 @@ window.addEventListener('mousemove', (event) => {
 // Renderer
 const canvas = document.querySelector('.webgl');
 const renderer = setupRenderer();   
+let composer;
+
+
 // renderer.setClearColor('#FFD94F');
 
 /**
@@ -48,6 +56,29 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 1
 camera.position.copy(curvePath.curve.getPointAt(0));
 camera.lookAt(curvePath.curve.getPointAt(0.99));
 scene.add(camera);
+
+const params = {
+    threshold: 0,
+    strength: .183,
+    radius: 0,
+    exposure: 1
+};
+
+const renderScene = new RenderPass( scene, camera );
+
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+bloomPass.threshold = params.threshold;
+bloomPass.strength = params.strength;
+bloomPass.radius = params.radius;
+
+const outputPass = new OutputPass();
+
+composer = new EffectComposer( renderer );
+composer.addPass( renderScene );
+composer.addPass( bloomPass );
+composer.addPass( outputPass );
+composer.renderTarget1.samples = 8;
+composer.renderTarget2.samples = 8;
 
 // PathState
 let positionAlongPathState = new PositionAlongPathState();
@@ -72,17 +103,41 @@ window.addEventListener('touchmove', (event) => {
 const spotLight = new THREE.SpotLight('#ffffff', 10);
 spotLight.position.set(3.8, 3.2, -3.5);
 // scene.add(spotLight);
+const rectLight = new THREE.RectAreaLight('#FFA000', 4, 10, 10);
+const rectLightHelper = new RectAreaLightHelper(rectLight);
+rectLight.position.set(7, 10, -2);
+rectLight.lookAt(new THREE.Vector3(0, 0, 0))
+
+// scene.add(rectLight);
 
 // Orbit controls
 // const controls = new OrbitControls(camera, canvas);
 // controls.enableDamping = true;
 
 // Debug
+
 // const gui = new dat.GUI();
-// gui.add(spotLight, 'intensity').min(0).max(10).step(.001).name('lightIntensity');
-// gui.add(spotLight.position, 'x').min(-5).max(10).step(.001).name('lightX');
-// gui.add(spotLight.position, 'y').min(-5).max(10).step(.001).name('lightY');
-// gui.add(spotLight.position, 'z').min(-5).max(10).step(.001).name('lightZ');
+
+
+// const bloomFolder = gui.addFolder( 'bloom' );
+
+// bloomFolder.add( params, 'threshold', 0.0, 1.0 ).onChange( function ( value ) {
+
+//     bloomPass.threshold = Number( value );
+
+// } );
+
+// bloomFolder.add( params, 'strength', 0.0, 3.0 ).onChange( function ( value ) {
+
+//     bloomPass.strength = Number( value );
+
+// } );
+
+// gui.add( params, 'radius', 0.0, 1.0 ).step( 0.01 ).onChange( function ( value ) {
+
+//     bloomPass.radius = Number( value );
+
+// } );
 
 // Window resize
 window.addEventListener('resize', () => {
@@ -125,5 +180,6 @@ function tick() {
     // controls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
+    composer.render();
 };
 tick();
